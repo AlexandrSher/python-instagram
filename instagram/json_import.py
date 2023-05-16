3f24fd354639fb969c32a4b3a701e3369ba8112d
Aliaksandr Chernak <aliaksandr_chernak@epam.com>
2023-05-16 13:39:15 +0200

    [noticket]: Test test

--- instagram/json_import.py
+++ instagram/json_import.py
@@ -2,9 +2,14 @@ try:
     import simplejson
 except ImportError:
     try:
-        import json as simplejson
+        import json as simplejson # just comment for test
     except ImportError:
         try:
             from django.utils import simplejson
         except ImportError:
             raise ImportError('A json library is required to use this python library')
+print("Hello")
+# I wanna check if it works
+
+
+# and Lint also
